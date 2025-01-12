ad4cdd5f3a4b01e6b3407b338954d2e999c6704f
brysonwebb98 <brysonwebb98@gmail.com>
2025-01-11 22:09:50 -0700

    Committing changes for W01 prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        string letter;
+        if (gradeInt >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        // FIRST PRINT STATEMENT
+        Console.WriteLine("Your letter grade is: " + letter);
+        string course;
+        if (gradeInt>= 70)
+        {
+            course = "passed";
+        }
+        else
+        {
+            course = "failed";
+        }
+        Console.WriteLine($"You {course} this course!");
     }
 }
