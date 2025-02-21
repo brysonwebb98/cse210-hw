ee1d0339c42c990ac9ec6de540c8b84adafa7f7b
brysonwebb98 <brysonwebb98@gmail.com>
2025-02-20 22:35:14 -0700

    Learning 04 Commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // ASSIGNMENTS CLASS 
+        Console.WriteLine("");
+        Assignment student1 = new Assignment("Bryson", "Math");
+        Console.WriteLine(student1.GetSummary());
+
+        // MATHASSIGNMENT CLASS
+        Console.WriteLine("");
+        MathAssignment student2 = new MathAssignment("Victoria", "History", "Section 7.3", "Problems 10-20");
+        Console.WriteLine(student2.GetSummary());
+        Console.WriteLine(student2.GetHomeworkList());
+
+        // WRITING ASSIGNMENT CLASS
+        Console.WriteLine("");
+        WritingAssignment student3 = new WritingAssignment("Tommy Smith", "Science", "How to Develop An Atom");
+        Console.WriteLine(student3.GetSummary());
+        Console.WriteLine(student3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,13 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    public WritingAssignment(string student, string topic, string title) : base(student, topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation()
+    {
+        string student = GetStudentName();
+        return $"{_title} by {student}";
+    }
+}
